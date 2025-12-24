13b0efaa13fbc8ff4cf1909bed5cfcdf283cce57
Yona Appletree <hypher@gmail.com>
2025-12-23 20:22:33 -0800

    Make 32-bit filetests script configurable with custom test path argument

--- scripts/32bit-filetests.sh
+++ scripts/32bit-filetests.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-cargo run -p cranelift-tools --bin clif-util -- test cranelift/filetests/filetests/32bit/
+# Default test path
+TEST_PATH="cranelift/filetests/filetests/32bit/"
+
+# If a test path is provided as an argument, use it instead
+if [ $# -gt 0 ]; then
+    TEST_PATH="$1"
+fi
+
+cargo run -p cranelift-tools --bin clif-util -- test "$TEST_PATH"
