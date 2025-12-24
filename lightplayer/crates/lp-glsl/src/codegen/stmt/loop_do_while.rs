2aefccdd98e9788be64a4e514edd92b9e4708acb
Yona Appletree <hypher@gmail.com>
2025-12-24 14:01:38 -0800

    lpc: fix block sealing order in do-while loops

--- lightplayer/crates/lp-glsl/src/codegen/stmt/loop_do_while.rs
+++ lightplayer/crates/lp-glsl/src/codegen/stmt/loop_do_while.rs
@@ -9,31 +9,33 @@ pub fn emit_loop_do_while_stmt(
     body: &Statement,
     condition: &Expr,
 ) -> Result<(), GlslError> {
+    // Create blocks (don't seal yet)
     let body_block = ctx.builder.create_block();
-    let header_block = ctx.builder.create_block();
+    let cond_block = ctx.builder.create_block();
     let exit_block = ctx.builder.create_block();
 
     ctx.loop_stack.push(crate::codegen::context::LoopContext {
-        continue_target: header_block,
+        continue_target: cond_block,
         exit_block,
     });
 
-    // Jump directly to body (do-while always executes once)
+    // Branch directly to body (do-while always executes once)
     ctx.emit_branch(body_block)?;
 
-    // Body
-    ctx.emit_block(body_block);
+    // Body: switch to but don't seal yet - will receive back edge from condition block
+    ctx.switch_to_block(body_block);
     ctx.emit_statement(body)?;
-    ctx.emit_branch(header_block)?;
+    ctx.emit_branch(cond_block)?;
 
-    // Header: evaluate condition
-    // Don't seal header yet - it will receive a back edge from body
-    ctx.switch_to_block(header_block);
+    // Condition: switch to but don't seal yet - will receive back edge from body
+    ctx.switch_to_block(cond_block);
     let condition_value = ctx.translate_expr(condition)?;
     ctx.emit_cond_branch(condition_value, body_block, exit_block)?;
+    // Now body_block is declared as successor - safe to seal both
 
-    // Now seal header block - all predecessors (initial jump + back edge from body) are known
-    ctx.seal_block(header_block);
+    // Seal blocks now that all predecessors are known
+    ctx.seal_block(body_block);
+    ctx.seal_block(cond_block);
 
     // Exit - seal immediately since all predecessors are known
     ctx.emit_block(exit_block);
